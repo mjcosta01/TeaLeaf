b7f64e89b1c1a237f5ff4e61144bc696705c96e4
Murilo <mjcosta01@outlook.com>
2021-05-06 17:17:26 -0400

    Konichiwa

---
+++ ProjectOsiris/Scripts/Eater_Demon.gd
@@ -0,0 +1,31 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+export var movespeed = 50
+var dir = false
+var vel = Vector2()
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+func get_direction():
+	if dir == false:
+		get_node("SpriteStuff").set_flip_h(false)
+		vel.x += movespeed
+	else:
+		get_node("SpriteStuff").set_flip_h(true)
+		vel.x -= movespeed
+	if is_on_wall():
+		dir = !dir
+func _physics_process(delta):
+	if vel.x == 0:
+		get_node("SpriteStuff").set_animation("Standby")
+	else:
+		get_node("SpriteStuff").set_animation("Walking")
+	get_direction()
+	vel = move_and_slide(vel, Vector2(0,-1))
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
