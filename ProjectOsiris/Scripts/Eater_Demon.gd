36753e22f7ff9e9ca75f0f8bf3d37c42774b4bd1
Murilo <mjcosta01@outlook.com>
2021-05-09 21:21:12 -0400

    Totally

--- ProjectOsiris/Scripts/Eater_Demon.gd
+++ ProjectOsiris/Scripts/Eater_Demon.gd
@@ -10,6 +10,7 @@ export var gravity = 500
 var dir = false
 var flag = false
 var vel = Vector2()
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
--- ProjectOsiris/Scripts/Johnny.gd
+++ ProjectOsiris/Scripts/Johnny.gd
@@ -16,19 +16,27 @@ var jumping = false
 
 func handle_collision(col: KinematicCollision2D):
 	var par = col.get_collider()
-	if !par.is_in_group("Monsters"):
-		return
-	if iframes == max_frames:
-		print("boop");
-		get_hurt();
-		vel.y = -100;
+	if par.is_in_group("Monsters"):
+		if col.position.y > position.y + 12:
+			if !in_trance:
+				par.queue_free();
+				vel.y = -300;
+				pass;
+		elif !in_trance:
+			print("boop");
+			get_hurt();
+			vel.y = -500;
 	return
-
+#while there are iframes still left, do not take more damage, otherwise take damage
 func get_hurt():
+	if in_trance:
+		pass
 	lose_health(1)
 	iframes = 0
 	get_node("SpriteStuff").visible = false
+	in_trance = true;
 
+#handle keyboard inputs
 func get_input():
 	vel.x = 0
 	var right = Input.is_action_pressed("right")
@@ -45,6 +53,7 @@ func get_input():
 		get_node("SpriteStuff").set_flip_h(true)
 		vel.x -= runspeed
 
+#physic loop for the player
 func _physics_process(delta):
 	if vel.x == 0:
 		get_node("SpriteStuff").set_animation("Standby")
@@ -67,24 +76,29 @@ func _process(delta):
 	if in_trance:
 		pass;
 	if iframes != max_frames:
-		in_trance = true;
-		yield(get_tree().create_timer(0.2), "timeout")
+		for i in 10:
+			yield(get_tree().create_timer(0.1), "timeout")
+			get_node("SpriteStuff").visible = !get_node("SpriteStuff").visible
+			in_trance = true;
 		iframes = max_frames
 		in_trance = false;
 	else:
 		get_node("SpriteStuff").visible = true
-		
+
+#gaining health raises hp
 func gain_health(hp):
 	curr_health += hp
 	if curr_health > max_health:
 		curr_health = max_health
 		
+#lose health function emits damage signal so UI can handle and subtracts HP by damage
 func lose_health(hp):
 	curr_health -= hp
 	emit_signal("took_damage", hp)
 	if curr_health <= 0:
 		die()
 		curr_health = 0
+		
 func die():
 	print("I died")
 	pass
