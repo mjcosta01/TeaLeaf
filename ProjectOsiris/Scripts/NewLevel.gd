fd129891a7df1b0b025cfd05deef675607a2cc3d
Kirsten <camlin@carolina.rr.com>
2021-05-11 22:04:30 -0400

    scripting
    trying to figure out the level change script

--- ProjectOsiris/Scripts/NewLevel.gd
+++ ProjectOsiris/Scripts/NewLevel.gd
@@ -6,12 +6,17 @@ var rng = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	Global.current_levelID = levelID
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
-func _level_progress():
-	pass
+#When the Player collides with the door, roll the random number and go to the next level, except the hub, not sure about the proper syntax, so it will be in comments for now
+#func _level_progress():
+#	pass
+
+
+func _on_ExitDoor_body_entered(body):
+	pass # Replace with function body.
